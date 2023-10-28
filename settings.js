10af74b92ba7ae7e06c76c1ceb980338cc510d2d
SubarGanzz <106585460+SubarGaming17@users.noreply.github.com>
2023-10-28 19:48:16 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -7,41 +7,41 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 /* ~~~~~~~~~ WEB API ~~~~~~~~~ */
-global.lol = '' // https://api.lolhuman.xyz
-global.xzn = '' // https://xnz.wtf
+global.lol = '068dd79b4eeb7102f9174159' // https://api.lolhuman.xyz
+global.xzn = '068dd79b4eeb7102f9174159' // https://xnz.wtf
 /* ~~~~~~~~~ SETTINGS OWNER ~~~~~~~~~ */
-global.numberowner = '6289513081052' // Owner Utama
-global.owner = ['6289513081052', '6283838530788'] // Owner Lainnya
-global.namaowner = 'Arxzy お-ぎ' // Nama Owner
-global.premium = ["6289513081052"] // Premium User
-global.nobot = '6289513081052'
+global.numberowner = '6287755567549' // Owner Utama
+global.owner = ['6287755567549', '6285856575305'] // Owner Lainnya
+global.namaowner = 'あ⁩ItsMeSubarシ' // Nama Owner
+global.premium = ["6287755567549"] // Premium User
+global.nobot = '6285856575305'
 /* ~~~~~~~~~ SETTINGS BOT ~~~~~~~~~ */
-global.namabot = 'Arxzy-MD' // NickBot
-global.typemenu = 'v2' // 'v1' > 'v2' > 'v3' > 'v4'
-global.typereply = 'v1' // 'v1' > 'v2'
-global.autoread = false // ReadChat
-global.autobio = false // AutoBio
+global.namabot = 'あ⁩SubarBotzོ' // NickBot
+global.typemenu = 'v3' // 'v1' > 'v2' > 'v3' > 'v4'
+global.typereply = 'v2' // 'v1' > 'v2'
+global.autoread = true // ReadChat
+global.autobio = true // AutoBio
 global.autoblok212 = true // AutoBlock Nomer +212
 global.onlyindo = false  // AutoBlock Selain Nomer Indo
 global.packname = 'Created By' // Watermark Sticker
-global.author = 'Arxzy ダ' // Watermark Sticker
+global.author = 'あ⁩SubarBotzོ' // Watermark Sticker
 /* ~~~~~~~~~ MESSAGES ~~~~~~~~~ */
 global.mess = {
-    done: 'Done ✅',
-    prem: 'Feature Only For User _*PREMIUM*_',
-    admin: 'Feature Only for _*Admin Group*_',
-    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
-    owner: 'Feature Only for _*owner*_',
-    group: 'Feature Only for _*Group Chat*_',
-    private: 'Feature Only for _*Private Chat*_',
-    wait: 'Wait a Moment, for Process',    
-    error: '_*Sorry Features Error*_',
+    done: '*Noh Selesai!!*',
+    prem: 'Emangnya Elu _*PREMIUM*_',
+    admin: 'Emangnya Elu _*Admin Grup*_',
+    botAdmin: '*Bot Jadiin Admin Dulu !!* !',
+    owner: 'Elu Bukan _*owner*_ Gw',
+    group: 'Cuman Bisa Di _*Grup!!*_',
+    private: 'Cuman Bisa Di _*Private Chat!!*_',
+    wait: '*Tunggu Dulu, Masih Proses Nih!!*',    
+    error: '_*Fiturnya Error B3G0*_',
 }
 /* ~~~~~~~~~ THUMBNAIL ~~~~~~~~~ */
-global.thumb = fs.readFileSync('./media/quoted.jpg')
+global.thumb = fs.readFileSync('./media/ItsMeSubar.jpg')
 global.menu = fs.readFileSync('./media/menu.jpg')
 /* ~~~~~~~~~ EDITS LINK ~~~~~~~~~ */
-global.link = 'https://chat.whatsapp.com/LfBvDxQujrLHihSRI6TCIZ'
+global.link = 'https://chat.whatsapp.com/GTcVDY465eVJg9XP2VvPKM'
 /* ~~~~~~~~~ END SYSTEM ~~~~~~~~~ */
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
